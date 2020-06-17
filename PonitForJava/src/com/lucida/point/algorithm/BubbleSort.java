a9498040611bb8b746c12dbfb8c81e9a49c0a09e
Lucidastar <843299570@qq.com>
2020-06-17 16:54:02 +0800

    完善冒泡排序和选择排序

--- PonitForJava/src/com/lucida/point/algorithm/BubbleSort.java
+++ PonitForJava/src/com/lucida/point/algorithm/BubbleSort.java
@@ -1,6 +1,7 @@
 package com.lucida.point.algorithm;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * 冒泡算法
@@ -10,7 +11,6 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr){
         int length = arr.length;
         int a = 0;
-        int b = 0;
         for (int i = length-1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j+1]){
@@ -27,9 +27,31 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 //        int arr[] = {4,67,23,45,21,23,1};
-        int arr[] = {8,7,6,5};
+        Integer arr[] = new Integer[]{8,7,6,5};
+//        BubbleSort.bubbleSort(arr);
         BubbleSort.bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
+
+//        Arrays.sort();
+//        Collections.sort();
     }
+
+    public  static  <T extends Comparable<T>> void bubbleSort(T[] array) {
+        for (int i = array.length-1; i >0; i --) {
+            boolean swapped = false;
+            for (int j = 0; j < i; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0 ) {
+                    T swap = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = swap;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
 
 }
