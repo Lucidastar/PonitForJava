c9af241150f2b844cd4abd87afba67b8a11dca1a
Lucidastar <843299570@qq.com>
2020-09-02 18:14:48 +0800

    添加hashMap的学习

---
+++ PonitForJava/src/com/lucida/point/other/Test.java
@@ -0,0 +1,7 @@
+package com.lucida.point.other;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
---
+++ PonitForJava/src/com/lucida/point/other/collection/HashMapStudy.java
@@ -0,0 +1,31 @@
+package com.lucida.point.other.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapStudy {
+    public static void main(String[] args) {
+
+        //hashMap的初始化
+        Map<String,String> map = new HashMap<>();
+
+        map.put("张三","张三");
+
+
+    }
+
+
+    public class Other{
+        public Other() {
+
+        }
+        public Other(int aa) {
+            this(aa,0);
+        }
+        public Other(int aa,int bb) {
+        }
+    }
+
+
+
+}
